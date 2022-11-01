eac2707d81ee90f2485190a93f2ea2251fc98900
MrHeisenberg12332121 <elistratowura135@gmail.com>
2022-11-01 19:38:28 +0300

    1
    1

---
+++ Lab 12/Lab B/Ex1/Starter/BinaryTree/IBinaryTree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BinaryTree
+{
+    // TODO - Define the IBinaryTree interface.
+    public interface IBinaryTree<TItem>
+    where TItem : IComparable<TItem>
+    {
+        void Add(TItem newItem);
+        void Remove(TItem itemToRemove);
+        void WalkTree();
+    }
+}
---
+++ Lab 9/Labfiles/Starter/Exercise 3/Grades.WPF/Controls/StudentPhoto.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Grades.WPF
+{
+    public partial class StudentPhoto : UserControl
+    {
+        #region Constructor
+        public StudentPhoto()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Storyboard
+        // TODO: Exercise 3: Task 2a: Handle mouse events to trigger the storyboards that animate the photograph
+        public void OnMouseEnter()
+        {
+            (this.Resources["sbMouseEnter"] as Storyboard).Begin();
+        }
+
+        public void OnMouseLeave()
+        {
+            (this.Resources["sbMouseLeave"] as Storyboard).Begin();
+        }
+        #endregion
+    }
+}
